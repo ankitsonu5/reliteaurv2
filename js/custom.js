5b0b93dcc8d87b44d012f237bedb03cb9b4b3c94
Ankit Srivastava <ankitsonu5@gmail.com>
2025-09-10 21:49:00 +0530

    u

--- js/custom.js
+++ js/custom.js
@@ -2,7 +2,37 @@ $(document).ready(function() {
     $(".readall-button").on("click", function(){
         $(this).toggleClass("active");
     });
+
+    // Function to toggle content
+    function toggleContent(element) {
+        var container = element.closest('.test-custom');
+        var content = container.find('.expandable-content');
+        var arrow = container.find('.arrow-icon');
+
+        if (content.is(':visible')) {
+            content.slideUp(300);
+            arrow.css('transform', 'rotate(0deg)');
+        } else {
+            content.slideDown(300);
+            arrow.css('transform', 'rotate(180deg)');
+        }
+    }
+
+    // H3 click functionality
+    $(".expandable-heading").on("click", function(){
+        toggleContent($(this));
+    });
+
+    // Arrow click functionality
+    $(".arrow-icon").on("click", function(){
+        toggleContent($(this));
+    });
 });
+
+    $(".readall-button2").on("click", function(){
+        $(this).toggleClass("active");
+    });
+ 
 
 $(document).ready(function() {
     $('#fadeButton').click(function() {
